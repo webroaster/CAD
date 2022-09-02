bc643b781f1be5ff7a16bfb9a57f67f3c624d7ac
web_roaster <84627640+webroaster@users.noreply.github.com>
2022-09-03 05:00:05 +0900

    [ feature ] | Googleアナリティクス設置

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,21 @@ import Layout from "../components/layout/Main";
 import Fonts from "../components/Fonts";
 import theme from "../lib/theme";
 import { AnimatePresence } from "framer-motion";
+import GoogleAnalytics from "../components/GoogleAnalytics";
 
 function MyApp({ Component, pageProps, router }) {
   return (
-    <ChakraProvider theme={theme}>
-      <Fonts />
-      <Layout router={router}>
-        <AnimatePresence exitBeforeEnter initial={true}>
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-      </Layout>
-    </ChakraProvider>
+    <>
+      <GoogleAnalytics />
+      <ChakraProvider theme={theme}>
+        <Fonts />
+        <Layout router={router}>
+          <AnimatePresence exitBeforeEnter initial={true}>
+            <Component {...pageProps} key={router.route} />
+          </AnimatePresence>
+        </Layout>
+      </ChakraProvider>
+    </>
   );
 }
 
